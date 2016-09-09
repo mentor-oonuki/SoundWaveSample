727b399ff3621e54e855e5b1847953da67cb0f96
mentor-oonuki <mentor.oonuki@gmail.com>
2016-09-10 01:33:56 +0900

    【初期登録】

---
+++ Assets/Scripts/WaveOutputter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class WaveOutputter : MonoBehaviour
+{
+    private float[] waveData_ = new float[1024];
+
+    void Update()
+    {
+        AudioListener.GetOutputData(waveData_, 1);
+        var volume = waveData_.Select(x => x * x).Sum() / waveData_.Length;
+        transform.localScale = Vector3.one * (volume + 1.0f);
+    }
+}
